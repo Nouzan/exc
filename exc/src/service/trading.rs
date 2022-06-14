883ad14f8405d798930ac12e87ca1f325480b062
Nouzan <clobraindie@outlook.com>
2022-06-14 04:20:25 +0000

    add `CheckOrderService`

--- exc/src/service/trading.rs
+++ exc/src/service/trading.rs
@@ -2,7 +2,7 @@ use futures::{future::BoxFuture, FutureExt};
 use tower::ServiceExt;
 
 use crate::{
-    types::trading::{CancelOrder, OrderId, Place, PlaceOrder},
+    types::trading::{CancelOrder, OrderId, Place, PlaceOrder, GetOrder, Order},
     ExchangeError,
 };
 
@@ -14,7 +14,7 @@ pub trait TradingService: ExchangeService<PlaceOrder> + ExchangeService<CancelOr
     fn place(&mut self, inst: &str, place: &Place) -> BoxFuture<'_, Result<OrderId, ExchangeError>>
     where
         Self: Sized + Send,
-        <Self as ExchangeService<PlaceOrder>>::Future: std::marker::Send,
+        <Self as ExchangeService<PlaceOrder>>::Future: Send,
     {
         let resp = ServiceExt::<PlaceOrder>::oneshot(
             ExchangeService::<PlaceOrder>::as_service_mut(self),
@@ -30,7 +30,7 @@ pub trait TradingService: ExchangeService<PlaceOrder> + ExchangeService<CancelOr
     fn cancel(&mut self, inst: &str, id: &OrderId) -> BoxFuture<'_, Result<(), ExchangeError>>
     where
         Self: Sized + Send,
-        <Self as ExchangeService<CancelOrder>>::Future: std::marker::Send,
+        <Self as ExchangeService<CancelOrder>>::Future: Send,
     {
         let resp = ServiceExt::<CancelOrder>::oneshot(
             ExchangeService::<CancelOrder>::as_service_mut(self),
@@ -44,3 +44,24 @@ pub trait TradingService: ExchangeService<PlaceOrder> + ExchangeService<CancelOr
 }
 
 impl<S> TradingService for S where S: ExchangeService<PlaceOrder> + ExchangeService<CancelOrder> {}
+
+/// Check order service.
+pub trait CheckOrderService: ExchangeService<GetOrder>{
+    /// Check the current status of an order.
+    fn check(&mut self, inst: &str, id: &OrderId) -> BoxFuture<'_, Result<Order, ExchangeError>>
+    where
+        Self: Sized + Send,
+        Self::Future: Send,
+    {
+        let resp = ServiceExt::oneshot(
+            ExchangeService::<GetOrder>::as_service_mut(self),
+            GetOrder {
+                instrument: inst.to_string(),
+                id: id.clone(),
+            },
+        );
+        async move { Ok(resp.await?.await?) }.boxed()
+    }
+}
+
+impl<S> CheckOrderService for S where S: ExchangeService<GetOrder> {}
