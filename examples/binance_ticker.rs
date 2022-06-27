15d32187615b8cb3a1bcd0443cd3f71bb1385d13
Nouzan <clobraindie@outlook.com>
2022-06-27 20:25:21 +0800

    robust binance-ticker example

--- examples/binance_ticker.rs
+++ examples/binance_ticker.rs
@@ -4,6 +4,7 @@ use exc::service::{
 };
 use exc_binance::Binance;
 use futures::StreamExt;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,10 +20,27 @@ async fn main() -> anyhow::Result<()> {
         .connect()
         .into_exchange()
         .into_subscribe_tickers();
-    let mut tickers = binance.subscribe_tickers("btcbusd").await?;
-    while let Some(t) = tickers.next().await {
-        let t = t?;
-        tracing::info!("{t}");
+
+    let mut revision = 0;
+    loop {
+        revision += 1;
+        match binance.subscribe_tickers("btcbusd").await {
+            Ok(mut tickers) => {
+                while let Some(t) = tickers.next().await {
+                    match t {
+                        Ok(t) => {
+                            tracing::info!("[{revision}]{t}");
+                        }
+                        Err(err) => {
+                            tracing::error!("[{revision}]stream error: {err}");
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                tracing::error!("[{revision}]request error: {err}");
+            }
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
     }
-    Ok(())
 }
