cf0dbd766eb78fcce742c9c822bda4812de8a872
Nouzan <clobraindie@outlook.com>
2022-06-30 21:19:27 +0800

    binance: finish order update parsing

--- exc-binance/src/websocket/protocol/frame/account.rs
+++ exc-binance/src/websocket/protocol/frame/account.rs
@@ -1,3 +1,4 @@
+use rust_decimal::Decimal;
 use serde::Deserialize;
 
 use crate::websocket::error::WsError;
@@ -25,8 +26,189 @@ pub enum AccountEvent {
         trade_ts: i64,
         /// Order.
         #[serde(rename = "o")]
-        order: serde_json::Value,
+        order: OrderUpdate,
     },
+}
+
+/// Order side.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum OrderSide {
+    /// Buy.
+    Buy,
+    /// Sell.
+    Sell,
+}
+
+/// Position side.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum PositionSide {
+    /// Long.
+    Long,
+    /// Short.
+    Short,
+}
+
+/// Order type.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum OrderType {
+    /// Market.
+    Market,
+    /// Limit.
+    Limit,
+    /// Stop.
+    Stop,
+    /// Take profit.
+    TakeProfit,
+    /// Liquidation.
+    Liquidation,
+}
+
+/// Time-in-force.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum TimeInForce {
+    /// Good-Till-Cancel.
+    Gtc,
+    /// Immdiate-Or-Cancel.
+    Ioc,
+    /// Fill-Or-Kill.
+    Fok,
+    /// Good-Till-Cancel (Post-Only)
+    Gtx,
+}
+
+/// Update kind.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum UpdateKind {
+    /// New.
+    New,
+    /// Cancelled.
+    Canceled,
+    /// Calculated.
+    Calculated,
+    /// Expired.
+    Expired,
+    /// Trade.
+    Trade,
+}
+
+/// Status.
+#[derive(Debug, Clone, Copy, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Status {
+    /// New.
+    New,
+    /// Parttially filled.
+    PartiallyFilled,
+    /// Filled.
+    Filled,
+    /// Cancelled.
+    Canceled,
+    /// Expired.
+    Expired,
+    /// New insurance.
+    NewInsurance,
+    /// New ADL.
+    NewAdl,
+}
+
+/// Order update.
+#[derive(Debug, Clone, Deserialize)]
+pub struct OrderUpdate {
+    /// Symbol.
+    #[serde(rename = "s")]
+    pub symbol: String,
+    /// Client id.
+    #[serde(rename = "c")]
+    pub client_id: String,
+    /// Order side.
+    #[serde(rename = "S")]
+    pub side: OrderSide,
+    /// Order type.
+    #[serde(rename = "o")]
+    pub order_type: OrderType,
+    /// Time-in-force.
+    #[serde(rename = "f")]
+    pub time_in_force: TimeInForce,
+    /// Size.
+    #[serde(rename = "q")]
+    pub size: Decimal,
+    /// Price. (FIXME: should this to be optional?)
+    #[serde(rename = "p")]
+    pub price: Decimal,
+    /// Cost.
+    #[serde(rename = "ap")]
+    pub cost: Decimal,
+    /// Trigger price.
+    #[serde(rename = "sp")]
+    pub trigger_price: Decimal,
+    /// Update kind.
+    #[serde(rename = "x")]
+    pub kind: UpdateKind,
+    /// Status.
+    #[serde(rename = "X")]
+    pub status: Status,
+    /// Order id.
+    #[serde(rename = "i")]
+    pub order_id: i64,
+    /// Last trade size.
+    #[serde(rename = "l")]
+    pub last_trade_size: Decimal,
+    /// Filled size.
+    #[serde(rename = "z")]
+    pub filled_size: Decimal,
+    /// Last trade price.
+    #[serde(rename = "L")]
+    pub last_trade_price: Decimal,
+    /// Fee asset.
+    #[serde(rename = "N")]
+    pub fee_asset: Option<String>,
+    /// Fee.
+    #[serde(rename = "n", default)]
+    pub fee: Decimal,
+    /// Trade timestamp.
+    #[serde(rename = "T")]
+    pub trade_ts: i64,
+    /// Trade id.
+    #[serde(rename = "t")]
+    pub trade_id: i64,
+    /// Bid equity.
+    #[serde(rename = "b")]
+    pub bid_equity: Decimal,
+    /// Ask equity.
+    #[serde(rename = "a")]
+    pub ask_equity: Decimal,
+    /// Maker.
+    #[serde(rename = "m")]
+    pub marker: bool,
+    /// Reduce-Only.
+    #[serde(rename = "R")]
+    pub reduce_only: bool,
+    /// Trigger type.
+    #[serde(rename = "wt")]
+    pub trigger_type: String,
+    /// Original order type.
+    #[serde(rename = "ot")]
+    pub original_order_type: OrderType,
+    /// Position side.
+    #[serde(rename = "ps")]
+    pub position_side: PositionSide,
+    /// Is triggered close.
+    #[serde(rename = "cp")]
+    pub is_triggered_close: Option<bool>,
+    /// Active price.
+    #[serde(rename = "AP")]
+    pub active_price: Option<Decimal>,
+    /// Cr.
+    #[serde(rename = "cr")]
+    pub cr: Option<Decimal>,
+    /// Profit and loss.
+    #[serde(rename = "rp")]
+    pub pnl: Decimal,
 }
 
 impl Nameable for AccountEvent {
