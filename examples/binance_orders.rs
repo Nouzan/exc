8050a85e751de7117440ce29f7a46f4894a1f215
Nouzan <clobraindie@outlook.com>
2022-08-28 18:53:24 +0800

    use otel

--- examples/binance_orders.rs
+++ examples/binance_orders.rs
@@ -6,7 +6,7 @@ use exc::{
 use exc_binance::Binance;
 use futures::StreamExt;
 use std::time::Duration;
-use tracing::instrument;
+use tracing::{instrument, Instrument};
 use tracing_subscriber::{prelude::*, EnvFilter, Registry};
 
 #[derive(Parser)]
@@ -24,30 +24,84 @@ async fn main() -> anyhow::Result<()> {
         std::env::var("RUST_LOG")
             .unwrap_or_else(|_| "error,binance_orders=debug,exc_binance=debug".into()),
     );
-    let tracer = opentelemetry_jaeger::new_pipeline()
-        .with_service_name("binance-orders-3")
-        .with_collector_endpoint("http://localhost:14268/api/traces")
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+        .with_trace_config(
+            opentelemetry::sdk::trace::config()
+                .with_sampler(opentelemetry::sdk::trace::Sampler::AlwaysOn)
+                .with_resource(opentelemetry::sdk::Resource::new(vec![
+                    opentelemetry::KeyValue::new("service.name", "binance-orders"),
+                ])),
+        )
         .install_batch(opentelemetry::runtime::Tokio)?;
     let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_writer(std::io::stderr)
-        .with_line_number(true)
-        .with_filter(env_filter);
+        .with_line_number(true);
 
     Registry::default()
+        .with(env_filter)
         .with(otel_layer)
         .with(fmt_layer)
         .try_init()?;
-    
-    tracing::info!("hello, world");
 
-    start().await?;
+    tokio::select! {
+        res = start() => {
+            res?;
+        },
+        _ = tokio::signal::ctrl_c() => {
+            tracing::info!("ctrl + c");
+        }
+    }
+    opentelemetry::global::shutdown_tracer_provider();
     Ok(())
+}
+
+#[instrument(skip(market))]
+async fn stream_worker(mut market: impl SubscribeTickersService, inst: &str) -> anyhow::Result<()> {
+    let mut revision = 0;
+    loop {
+        revision += 1;
+        match market
+            .subscribe_tickers(inst)
+            .instrument(tracing::info_span!("subscribe", inst))
+            .await
+        {
+            Ok(mut stream) => {
+                while let Some(ticker) = stream
+                    .next()
+                    .instrument(tracing::info_span!("fetch_next", inst))
+                    .await
+                {
+                    let span = tracing::info_span!("hanlde_ticker", inst);
+                    let _enter = span.enter();
+                    match ticker {
+                        Ok(ticker) => {
+                            if !ticker.size.is_zero() {
+                                tracing::info!(rev = revision, %inst, "{ticker}");
+                            }
+                        }
+                        Err(err) => {
+                            tracing::error!(rev = revision, %inst, "stream error: {err}");
+                            break;
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                tracing::error!(
+                    rev = revision,
+                    %inst,
+                    "request new stream error: {err}"
+                );
+            }
+        }
+    }
 }
 
 #[instrument]
 async fn start() -> anyhow::Result<()> {
-    tracing::info!("start!");
     let args = Args::from_args();
     let key = serde_json::from_str(&args.binance_key)?;
 
@@ -65,47 +119,25 @@ async fn start() -> anyhow::Result<()> {
         .into_exc();
 
     let inst = args.inst.clone();
-    let mut market = binance
+    let market = binance
         .clone()
         .into_subscribe_tickers()
         .into_retry(Duration::from_secs(30));
-    tokio::spawn(async move {
-        let mut revision = 0;
-        loop {
-            revision += 1;
-            match market.subscribe_tickers(&inst).await {
-                Ok(mut stream) => {
-                    while let Some(ticker) = stream.next().await {
-                        match ticker {
-                            Ok(ticker) => {
-                                if !ticker.size.is_zero() {
-                                    tracing::info!(rev = revision, %inst, "{ticker}");
-                                }
-                            }
-                            Err(err) => {
-                                tracing::error!(rev = revision, %inst, "stream error: {err}");
-                                break;
-                            }
-                        }
-                    }
-                }
-                Err(err) => {
-                    tracing::error!(
-                        rev = revision,
-                        %inst,
-                        "request new stream error: {err}"
-                    );
+
+    let (_cancel_tx, cancel_rx) = tokio::sync::oneshot::channel::<()>();
+    tokio::spawn(
+        async move {
+            tokio::select! {
+                _ = stream_worker(market, &inst) => {
+
+                },
+                _ = cancel_rx => {
+
                 }
             }
         }
-    });
-
-    tokio::spawn(async move {
-        let _ = tokio::signal::ctrl_c().await;
-        tracing::info!("`ctrl + c`");
-        opentelemetry::global::shutdown_tracer_provider();
-        std::process::exit(0);
-    });
+        .in_current_span(),
+    );
 
     let mut revision = 0;
     let mut orders = ExcService::<SubscribeOrders>::into_retry(binance, Duration::from_secs(30));
