5e7a67dc8947363364beea707d0e7db286cf26e0
Nouzan <clobraindie@outlook.com>
2022-09-22 20:49:49 +0800

    use the right level filter

--- examples/okx_ws_trading.rs
+++ examples/okx_ws_trading.rs
@@ -17,7 +17,8 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::fmt()
         .with_writer(std::io::stderr)
         .with_env_filter(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "okx_ws_trade=debug,exc_okx=trace".into()),
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "info,okx_ws_trading=debug,exc_okx=trace".into()),
         ))
         .init();
 
