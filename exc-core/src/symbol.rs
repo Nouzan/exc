cf79f450c23335f0a99b9df2e9414777dc9fb843
Nouzan <clobraindie@outlook.com>
2022-12-25 13:53:15 +0800

    symbol: add creation methods

--- exc-core/src/symbol.rs
+++ exc-core/src/symbol.rs
@@ -1,7 +1,8 @@
 use positions::{prelude::Str, Asset, ParseSymbolError};
+use rust_decimal::Decimal;
 use std::{borrow::Borrow, fmt, str::FromStr};
 use thiserror::Error;
-use time::{macros::format_description, parsing::Parsable, Date};
+use time::{formatting::Formattable, macros::format_description, parsing::Parsable, Date};
 
 use crate::Symbol;
 
@@ -56,8 +57,13 @@ impl ExcSymbol {
     pub const SEP: char = '-';
 
     /// Get date format.
-    pub fn date_format() -> impl Parsable {
+    fn date_format() -> impl Parsable {
         format_description!("[year][month][day]")
+    }
+
+    /// Get date format for formatting.
+    fn formatting_date_format() -> impl Formattable {
+        format_description!("[year repr:last_two][month][day]")
     }
 
     /// Exchange.
@@ -66,8 +72,81 @@ impl ExcSymbol {
         Some(prefix)
     }
 
-    /// Try creating from symbol.
-    pub fn new(symbol: &Symbol) -> Option<Self> {
+    /// Create a symbol for spot.
+    pub fn spot(base: &Asset, quote: &Asset) -> Self {
+        Self(Symbol::spot(base, quote))
+    }
+
+    /// Create a symbol for margin.
+    pub fn margin(exchange: &str, base: &Asset, quote: &Asset) -> Self {
+        Self(Symbol::derivative(exchange, &format!("{base}-{quote}")).expect("must be valid"))
+    }
+
+    /// Create a symbol for perpetual.
+    pub fn perpetual(exchange: &str, base: &Asset, quote: &Asset) -> Self {
+        Self(
+            Symbol::derivative(exchange, &format!("{base}-{quote}-{}", Self::PERPETUAL))
+                .expect("must be valid"),
+        )
+    }
+
+    /// Create a symbol for futures.
+    /// Return `None` if `date` cannot be parsed by the date format.
+    pub fn futures(exchange: &str, base: &Asset, quote: &Asset, date: Date) -> Option<Self> {
+        let format = Self::formatting_date_format();
+        let date = date.format(&format).ok()?;
+        Some(Self(
+            Symbol::derivative(exchange, &format!("{base}-{quote}-{}{date}", Self::FUTURES))
+                .expect("must be valid"),
+        ))
+    }
+
+    /// Create a symbol for put options.
+    /// Return `None` if `date` cannot be parsed by the date format.
+    pub fn put(
+        exchange: &str,
+        base: &Asset,
+        quote: &Asset,
+        date: Date,
+        price: Decimal,
+    ) -> Option<Self> {
+        let format = Self::formatting_date_format();
+        let date = date.format(&format).ok()?;
+        Some(Self(
+            Symbol::derivative(
+                exchange,
+                &format!("{base}-{quote}-{}{date}{}{price}", Self::OPTIONS, Self::PUT),
+            )
+            .expect("must be valid"),
+        ))
+    }
+
+    /// Create a symbol for call options.
+    /// Return `None` if `date` cannot be parsed by the date format.
+    pub fn call(
+        exchange: &str,
+        base: &Asset,
+        quote: &Asset,
+        date: Date,
+        price: Decimal,
+    ) -> Option<Self> {
+        let format = Self::formatting_date_format();
+        let date = date.format(&format).ok()?;
+        Some(Self(
+            Symbol::derivative(
+                exchange,
+                &format!(
+                    "{base}-{quote}-{}{date}{}{price}",
+                    Self::OPTIONS,
+                    Self::CALL
+                ),
+            )
+            .expect("must be valid"),
+        ))
+    }
+
+    /// From symbol.
+    pub fn from_symbol(symbol: &Symbol) -> Option<Self> {
         if symbol.is_spot() {
             Some(Self(symbol.clone()))
         } else if let Some((_, sym)) = symbol.as_derivative() {
@@ -89,7 +168,8 @@ impl ExcSymbol {
                         let (date, opts) = extra.split_at(6);
                         let format = Self::date_format();
                         Date::parse(&format!("20{}", date), &format).ok()?;
-                        let (opts, _price) = opts.split_at(1);
+                        let (opts, price) = opts.split_at(1);
+                        Decimal::from_str_exact(price).ok()?;
                         match opts {
                             Self::PUT => {}
                             Self::CALL => {}
@@ -171,7 +251,7 @@ impl FromStr for ExcSymbol {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let symbol = Symbol::from_str(s)?;
-        Self::new(&symbol).ok_or(ParseExcSymbolError::InvalidFormat)
+        Self::from_symbol(&symbol).ok_or(ParseExcSymbolError::InvalidFormat)
     }
 }
 
@@ -181,8 +261,17 @@ impl Borrow<Symbol> for ExcSymbol {
     }
 }
 
+impl From<ExcSymbol> for Symbol {
+    fn from(symbol: ExcSymbol) -> Self {
+        symbol.0
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use rust_decimal_macros::dec;
+    use time::macros::date;
+
     use super::*;
 
     #[test]
@@ -246,6 +335,93 @@ mod tests {
     #[test]
     fn test_put_options() {
         let symbol: ExcSymbol = ":BTC-USDT-O221230P17000".parse().unwrap();
+        assert!(!symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (
+                Asset::BTC,
+                Asset::USDT,
+                SymbolType::Options(Str::new("221230"), OptionsType::Put(Str::new("17000"))),
+            )
+        );
+    }
+
+    #[test]
+    fn test_spot_creation() {
+        let symbol = ExcSymbol::spot(&Asset::BTC, &Asset::USDT);
+        assert!(symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (Asset::BTC, Asset::USDT, SymbolType::Spot)
+        );
+    }
+
+    #[test]
+    fn test_margin_creation() {
+        let symbol = ExcSymbol::margin("", &Asset::BTC, &Asset::USDT);
+        assert!(!symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (Asset::BTC, Asset::USDT, SymbolType::Margin)
+        );
+    }
+
+    #[test]
+    fn test_futures_creation() {
+        let symbol =
+            ExcSymbol::futures("", &Asset::BTC, &Asset::USDT, date!(2022 - 12 - 30)).unwrap();
+        assert!(!symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (
+                Asset::BTC,
+                Asset::USDT,
+                SymbolType::Futures(Str::new("221230"))
+            )
+        );
+    }
+
+    #[test]
+    fn test_perpetual_creation() {
+        let symbol = ExcSymbol::perpetual("", &Asset::BTC, &Asset::USDT);
+        assert!(!symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (Asset::BTC, Asset::USDT, SymbolType::Perpetual,)
+        );
+    }
+
+    #[test]
+    fn test_call_options_creation() {
+        let symbol = ExcSymbol::call(
+            "",
+            &Asset::BTC,
+            &Asset::USDT,
+            date!(2022 - 12 - 30),
+            dec!(17000),
+        )
+        .unwrap();
+        assert!(!symbol.0.is_spot());
+        assert_eq!(
+            symbol.to_parts(),
+            (
+                Asset::BTC,
+                Asset::USDT,
+                SymbolType::Options(Str::new("221230"), OptionsType::Call(Str::new("17000"))),
+            )
+        );
+    }
+
+    #[test]
+    fn test_put_options_creation() {
+        let symbol = ExcSymbol::put(
+            "",
+            &Asset::BTC,
+            &Asset::USDT,
+            date!(2022 - 12 - 30),
+            dec!(17000),
+        )
+        .unwrap();
         assert!(!symbol.0.is_spot());
         assert_eq!(
             symbol.to_parts(),
