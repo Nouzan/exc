2fa79ed53f91b59b9cfc0c8b56bab47d06785b8d
Nouzan <clobraindie@outlook.com>
2023-05-14 10:21:50 +0800

    exc: relax the bound of `SubscribeTradesService`

--- examples/examples/box_exc_service.rs
+++ examples/examples/box_exc_service.rs
@@ -1,25 +1,54 @@
 use std::str::FromStr;
 
 use clap::Parser;
-use exc::{core::types::Ticker, ExcExt, SubscribeTickersService};
-use futures::{Stream, TryStreamExt};
+use exc::{
+    core::types::{SubscribeTickers, SubscribeTrades, Ticker, Trade},
+    ExcExt, IntoExc, SubscribeTickersService, SubscribeTradesService,
+};
+use futures::{Stream, StreamExt, TryStreamExt};
+use tower::{layer::layer_fn, ServiceExt};
 
 struct Exchange {
     ticker: Box<dyn SubscribeTickersService>,
+    trade: Box<dyn SubscribeTradesService>,
 }
 
 impl Exchange {
     fn binance() -> Self {
-        let binance = exc::Binance::spot().connect_exc().into_subscribe_tickers();
+        // We cannot clone `Binance` here because the user may subscribe to both ticker and trade
+        // channels which will undelryingly subscribe trade channel twice.
         Self {
-            ticker: Box::new(binance),
+            ticker: Box::new(exc::Binance::spot().connect_exc().into_subscribe_tickers()),
+            trade: Box::new(exc::Binance::spot().connect_exc()),
         }
     }
 
     fn okx() -> Self {
-        let okx = exc::Okx::endpoint().connect_exc();
+        // We cannot clone `Okx` here because the user may subscribe to both ticker and trade
+        // channels which will undelryingly subscribe to ticker channel twice.
+        let trade_svc = exc::Okx::endpoint()
+            .connect_exc()
+            .layer(&layer_fn(|svc: exc::Okx| {
+                svc.into_exc()
+                    // We use `adapt` method to convert the request type to `SubscribeTickers`.
+                    .adapt::<SubscribeTickers>()
+                    .map_request(|req: SubscribeTrades| SubscribeTickers {
+                        instrument: req.instrument,
+                    })
+                    .map_response(|res| {
+                        res.map_ok(|t| Trade {
+                            ts: t.ts,
+                            price: t.last,
+                            size: t.size,
+                            // Just as an example, it is not recommended.
+                            buy: t.buy.unwrap_or(true),
+                        })
+                        .boxed()
+                    })
+            }));
         Self {
-            ticker: Box::new(okx),
+            ticker: Box::new(exc::Okx::endpoint().connect_exc()),
+            trade: Box::new(trade_svc),
         }
     }
 
@@ -27,8 +56,14 @@ impl Exchange {
         &mut self,
         inst: impl AsRef<str>,
     ) -> anyhow::Result<impl Stream<Item = exc::Result<Ticker>>> {
-        let stream = self.ticker.subscribe_tickers(inst.as_ref()).await?;
-        Ok(stream)
+        Ok(self.ticker.subscribe_tickers(inst.as_ref()).await?)
+    }
+
+    async fn subscribe_trades(
+        &mut self,
+        inst: impl AsRef<str>,
+    ) -> anyhow::Result<impl Stream<Item = exc::Result<Trade>>> {
+        Ok(self.trade.subscribe_trades(inst.as_ref()).await?)
     }
 }
 
@@ -56,9 +91,21 @@ struct Cli {
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::try_parse()?;
     let mut exchange: Exchange = cli.exchange.parse()?;
-    let mut stream = exchange.subscribe_tickers(&cli.inst).await?;
-    while let Some(ticker) = stream.try_next().await? {
-        println!("{ticker}");
-    }
+    let mut stream = exchange.subscribe_tickers(cli.inst.clone()).await?;
+    let ticker_handle = tokio::spawn(async move {
+        while let Some(ticker) = stream.try_next().await? {
+            println!("ticker: {ticker}");
+        }
+        anyhow::Result::<_>::Ok(())
+    });
+    let mut stream = exchange.subscribe_trades(cli.inst.clone()).await?;
+    let trade_handle = tokio::spawn(async move {
+        while let Some(trade) = stream.try_next().await? {
+            println!("trade: {trade}");
+        }
+        anyhow::Result::<_>::Ok(())
+    });
+    ticker_handle.await??;
+    trade_handle.await??;
     Ok(())
 }
--- exc/src/util/trade.rs
+++ exc/src/util/trade.rs
@@ -1,13 +1,21 @@
-use tower::{util::Oneshot, ServiceExt};
-
-use crate::core::types::trade::SubscribeTrades;
-
 use crate::ExcService;
+use exc_core::types::{SubscribeTrades, TradeStream};
+use futures::future::BoxFuture;
+use futures::FutureExt;
+use tower::ServiceExt;
 
 /// Subscribe trades service.
-pub trait SubscribeTradesService: ExcService<SubscribeTrades> {
+pub trait SubscribeTradesService {
     /// Subscribe trades.
-    fn subscribe_trades(&mut self, inst: &str) -> Oneshot<&mut Self, SubscribeTrades>
+    fn subscribe_trades(&mut self, inst: &str) -> BoxFuture<'_, crate::Result<TradeStream>>;
+}
+
+impl<S> SubscribeTradesService for S
+where
+    S: ExcService<SubscribeTrades> + Send,
+    S::Future: Send,
+{
+    fn subscribe_trades(&mut self, inst: &str) -> BoxFuture<'_, crate::Result<TradeStream>>
     where
         Self: Sized,
     {
@@ -17,7 +25,6 @@ pub trait SubscribeTradesService: ExcService<SubscribeTrades> {
                 instrument: inst.to_string(),
             },
         )
+        .boxed()
     }
 }
-
-impl<S> SubscribeTradesService for S where S: ExcService<SubscribeTrades> {}
