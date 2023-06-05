de0ce89fe875bbcd457e8fb07222fbc85b50b814
Nouzan <clobraindie@outlook.com>
2023-06-05 17:06:25 +0800

    chore: re-export core types

--- exc/src/types/mod.rs
+++ exc/src/types/mod.rs
@@ -1,4 +1,13 @@
-pub use crate::core::types::Period;
+pub use self::instrument::{GetInstrument, InstrumentMeta};
+pub use crate::core::types::{Period, PeriodKind};
+// These types are going to be replaced by theirs
+// higer-level versions in the future.
+pub use crate::core::types::{
+    BidAsk, BidAskStream, CancelOrder, Canceled, Candle, CandleStream, GetOrder, Order, OrderId,
+    OrderKind, OrderState, OrderStatus, OrderStream, OrderTrade, OrderUpdate, Place, PlaceOrder,
+    PlaceOrderOptions, Placed, QueryCandles, SubscribeBidAsk, SubscribeOrders, SubscribeTickers,
+    Ticker, TickerStream, TimeInForce,
+};
 
 /// Instrument.
 pub mod instrument;
