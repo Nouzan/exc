91060dc66af6431fec26ae8281227bc667e74fdf
Nouzan <clobraindie@outlook.com>
2023-10-25 15:37:33 +0800

    core: finish the definition of `MakeExchange`

--- exc-core/src/exchange.rs
+++ exc-core/src/exchange.rs
@@ -1,7 +1,21 @@
-use exc_make::{MakeInstruments, MakeTickers};
+use exc_make::{
+    MakeCancelOrder, MakeCheckOrder, MakeInstruments, MakePlaceOrder, MakeSubscribeOrders,
+    MakeTickers,
+};
 
 /// Make a exchange service.
-pub trait MakeExchange: MakeInstruments + MakeTickers {
+pub trait MakeExchange: MakeInstruments + MakeTickers + MakeTrading {
     /// Name of the exchange.
     fn name(&self) -> &str;
+}
+
+/// Make a trading service.
+pub trait MakeTrading:
+    MakePlaceOrder + MakeCancelOrder + MakeCheckOrder + MakeSubscribeOrders
+{
+}
+
+impl<M> MakeTrading for M where
+    M: MakePlaceOrder + MakeCancelOrder + MakeCheckOrder + MakeSubscribeOrders
+{
 }
