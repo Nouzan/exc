f4af839789b4338a10cb14c254fdd1da2c62ea2b
Nouzan <clobraindie@outlook.com>
2023-10-25 19:40:35 +0800

    core: move `PollInstruments` to core
    - And add `BinanceExchange`

--- exc-core/src/util/mod.rs
+++ exc-core/src/util/mod.rs
@@ -4,4 +4,8 @@ pub mod period;
 /// Create a service to subscribe tickers from subscribe trades and bid/ask.
 pub mod trade_bid_ask;
 
+/// Poll instruments.
+#[cfg(feature = "poll")]
+pub mod poll_instruments;
+
 pub use period::{trunc, PeriodExt};
