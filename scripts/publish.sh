ee66b6723ac5f714cbf8605796d3c22413c55561
Nouzan <clobraindie@outlook.com>
2023-11-25 23:14:54 +0800

    chore: add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+cargo publish -p exc-symbol $@ && \
+cargo publish -p exc-service $@ && \
+cargo publish -p exc-types $@ && \
+cargo publish -p exc-make $@ && \
+cargo publish -p exc-core $@ && \
+cargo publish -p exc-binance $@ && \
+cargo publish -p exc-okx $@ && \
+cargo publish -p exc $@
