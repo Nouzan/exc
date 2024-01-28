eb2d63729a7ff08ea948f31341c906bdef73a183
Nouzan <clobraindie@outlook.com>
2024-01-28 10:43:06 +0800

    okx: add query str support for inst tag

--- exc-okx/src/http/types/adaptations/instruments.rs
+++ exc-okx/src/http/types/adaptations/instruments.rs
@@ -1,5 +1,6 @@
 use exc_core::types::instrument::{FetchInstruments, InstrumentMeta};
-use exc_core::{Adaptor, ExchangeError};
+use exc_core::{Adaptor, ExchangeError, Str};
+use serde::Deserialize;
 
 use crate::http::types::request::instruments::Instruments;
 use crate::http::types::request::Get;
@@ -14,8 +15,24 @@ impl Adaptor<FetchInstruments> for HttpRequest {
     where
         Self: Sized,
     {
+        #[derive(Debug, Deserialize, Default)]
+        struct Params {
+            family: Option<Str>,
+            uly: Option<Str>,
+        }
+
+        let (tag, params) = req
+            .tag
+            .split_once('?')
+            .map(|(ty, params)| serde_qs::from_str::<Params>(params).map(|p| (Str::new(ty), p)))
+            .transpose()
+            .map_err(anyhow::Error::from)?
+            .unwrap_or_else(|| (req.tag, Params::default()));
+
         let req = Self::Get(Get::Instruments(Instruments {
-            inst_type: req.tag,
+            inst_type: tag,
+            inst_family: params.family,
+            uly: params.uly,
             ..Default::default()
         }));
         Ok(req)
