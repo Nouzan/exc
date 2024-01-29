c577e6918c1dd84a1bdc4ea7498b54491e3ebc61
Nouzan <clobraindie@outlook.com>
2024-01-29 16:03:56 +0800

    okx: add example for subscribing custom channel

--- exc-okx/examples/okx_channel.rs
+++ exc-okx/examples/okx_channel.rs
@@ -1,9 +1,6 @@
 use std::env;
 
-use exc_okx::{
-    websocket::types::messages::{event::OkxOptionSummary, Args},
-    Okx, OkxRequest,
-};
+use exc_okx::{websocket::types::messages::Args, Okx, OkxRequest};
 use futures::StreamExt;
 use tower::{Service, ServiceExt};
 
@@ -14,21 +11,22 @@ async fn main() -> anyhow::Result<()> {
         .with_writer(std::io::stderr)
         .with_env_filter(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "error,exc_okx=debug,okx_option_summary=info".into()),
+                .unwrap_or_else(|_| "error,exc_okx=debug,okx_channel=info".into()),
         ))
         .init();
     let mut okx = Okx::endpoint().connect();
     let mut stream = okx
         .ready()
         .await?
-        .call(OkxRequest::subscribe(Args::subscribe_option_summary(
-            &inst_family,
+        .call(OkxRequest::subscribe(Args::subscribe_channel(
+            "estimated-price",
+            [("instFamily", inst_family.as_str()), ("instType", "OPTION")],
         )))
         .await?
         .ws()?
         .into_stream()?;
     while let Some(frame) = stream.next().await.transpose()? {
-        let Some(datas) = frame.to_deserialized_changes::<OkxOptionSummary>() else {
+        let Some(datas) = frame.to_deserialized_changes::<serde_json::Value>() else {
             tracing::warn!("not a change: {frame:?}");
             continue;
         };
