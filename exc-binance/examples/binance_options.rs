08a766d7dea122219852eba15f781c9b3e934645
Nouzan <clobraindie@outlook.com>
2024-02-11 15:01:03 +0800

    binance: add CHANNEL env

--- exc-binance/examples/binance_options.rs
+++ exc-binance/examples/binance_options.rs
@@ -15,6 +15,7 @@ async fn main() -> anyhow::Result<()> {
             std::env::var("RUST_LOG").unwrap_or_else(|_| "error,binance_options=debug".into()),
         ))
         .init();
+    let channel = std::env::var("CHANNEL").unwrap_or("index".to_string());
     let inst = std::env::var("INST")?;
     let mut api = Binance::european_options()
         .ws_keep_alive_timeout(Duration::from_secs(30))
@@ -22,29 +23,22 @@ async fn main() -> anyhow::Result<()> {
     api.ready().await?;
     let mut stream = api
         .call(Request::Ws(WsRequest::subscribe_stream(
-            Name::new("index").with_inst(&inst),
+            Name::new(&channel).with_inst(&inst),
         )))
         .await?
         .into_stream::<serde_json::Value>()
         .unwrap()
         .boxed();
-    let jh = tokio::spawn(async move {
-        while let Some(data) = stream.next().await {
-            match data {
-                Ok(data) => {
-                    tracing::info!("data={data:#?}");
-                }
-                Err(err) => {
-                    tracing::error!("error={err}");
-                    break;
-                }
+    while let Some(data) = stream.next().await {
+        match data {
+            Ok(data) => {
+                tracing::info!("data={data:#?}");
+            }
+            Err(err) => {
+                tracing::error!("error={err}");
+                break;
             }
         }
-    });
-
-    tokio::time::sleep(Duration::from_secs(30)).await;
-    jh.abort();
-
-    tokio::time::sleep(Duration::from_secs(30)).await;
+    }
     Ok(())
 }
